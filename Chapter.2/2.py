bde4bfad0a24f915ed51575a4d59c3b9a8b55b88
alstn2468 <alstn2468_@naver.com>
2018-04-25 20:45:07 +0900

    2018-04-25

---
+++ Chapter.2/2.py
@@ -0,0 +1,26 @@
+# 문자열
+"Life is too short, You need Python"
+"a"
+"123"
+
+# 큰따옴표(")로 양쪽 둘러싸기
+"Hello World"
+
+# 작은따옴표(')로 양쪽 둘러싸기
+'Python is fun'
+
+# 큰따옴표 3개를 연속(""")으로 써서 양쪽 둘러싸기
+"""Life is too short, You need python"""
+
+# 작은따옴표 3개를 연속(''')으로 써서 양쪽 둘러싸기
+'''Life is too short, You need python'''
+
+# 문자열에 작은따옴표(')포함시키기
+food = "Python's favorite food is perl"
+
+# 문자열에 큰따옴표(")포함시키기
+say = '"Python is very easy." he says.'
+
+# \(백슬래시)를 이용해서 작은따옴표(')와 큰따옴표(")를 문자열에 포함시키기
+food = 'Python\'s favorite food is perl'
+say = "\"Python is very easy.\"he says."
