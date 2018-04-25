e2967b1e8851129a03eaa418302a7d8dd2d06d88
alstn2468 <alstn2468_@naver.com>
2018-04-25 15:26:57 +0900

    2018-04-25

---
+++ Chapter.1/2.py
@@ -0,0 +1,11 @@
+# simple.py
+
+languages = ['python', 'perl', 'c', 'java']
+
+for lang in languages :
+    if lang in ['python', 'perl'] :
+        print("%6s need interpreter" % lang)
+    elif lang in ['c', 'java'] :
+        print("%6s need compiler" % lang)
+    else :
+        print("should not reach here")
