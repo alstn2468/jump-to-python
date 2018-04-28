64a535a8d1ab086a1d39ab8aa0bf82d52ab7fb52
alstn2468 <alstn2468_@naver.com>
2018-04-28 18:23:27 +0900

    2018-04-28

--- Chapter.3/예제/11.py
+++ Chapter.3/예제/11.py
@@ -1,1 +1,12 @@
 # for와 함께 자주 사용하는 함수 range함수
+
+a = range(10)
+a # range(0, 10)
+a = range(1, 11)
+a # range(1, 11)
+
+# range 함수의 예시
+sum = 0
+for i in range(1, 11) :
+    sum = sum + i
+print(sum) # 55
---
+++ Chapter.3/예제/13.py
@@ -0,0 +1,6 @@
+# for와 range를 이용한 구구단
+
+for i in range(2, 10) :
+    for j in range(1, 10) :
+        print(i * j, end = ' ')
+    print(' ')
