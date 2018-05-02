6c052db86462196df089351be95a8d69b3836827
alstn2468 <alstn2468_@naver.com>
2018-05-02 20:58:49 +0900

    2018-05-02

--- Chapter.5/예제/1_Class/16.py
+++ Chapter.5/예제/1_Class/16.py
@@ -1,2 +1,34 @@
 # '박씨네 집' 클래스 완성하기
 class HousePark :
+    lastname = '박'
+    def __init__(self, name) :
+        self.fullname = self.lastname + name
+
+    def travel(self, where) :
+        print('%s, %s여행을 가다.' % (self.fullname, where))
+
+    def love(self, other) :
+        print('%s, %s 사랑에 빠졌네' % (self.fullname, other.fullname))
+
+    def figth(self, other) :
+        print('%s, %s 싸우네' % (self.fullname, other.fullname))
+
+    def __add__(self, other) :
+        print('%s, %s 결혼했네' % (self.fullname, other.fullname))
+
+    def __sub__(self, other) :
+        print('%s, %s 이혼했네' % (self.fullname, other.fullname))
+
+class HouseKim(HousePark) :
+    lastname = '김'
+    def travel(self, where, day) :
+        print('%s, %s여행 %d일 가네.' % (self.fullname, where, day))
+
+pey = HousePark('응용')
+juliet = HouseKim('줄리엣')
+pey.travel('부산')
+juliet.travel('부산', 3)
+pey.love(juliet)
+pey + juliet
+pey.figth(juliet)
+pey - juliet
