d14ee72237d1829831cc3074b8211b06418e3c43
alstn2468 <alstn2468_@naver.com>
2018-05-07 00:10:31 +0900

    2018-05-07

--- Chapter.6/예제/3.py
+++ Chapter.6/예제/3.py
@@ -1,1 +1,11 @@
 # 게시판 페이징하기
+def getTotalPage(m, n) :
+    if m % n == 0 :
+        return m // n
+    else :
+        return m // n + 1
+
+print(getTotalPage(5, 10)) # 1
+print(getTotalPage(15, 10)) # 2
+print(getTotalPage(25, 10)) # 3
+print(getTotalPage(30, 10)) # 3
