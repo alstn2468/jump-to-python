3044452eb89952c925e4be319bbdf59d1610827a
alstn2468 <alstn2468_@naver.com>
2018-05-13 15:06:32 +0900

    2018-05-13

--- Chapter.6/Practice/1.py
+++ Chapter.6/Practice/1.py
@@ -2,20 +2,20 @@
 문자열을 입력 받아 같은 문자가 연속적으로 반복되는 경우에
 그 반복 횟수를 표시해 문자열을 압축하여 표시해 보자.
 
-입력 예시 : aaabbcccccca
-출력 예시 : a3b2c6a1
+입력 예시 : aaabbcharcharcharcharcharchara
+출력 예시 : a3b2char6a1
 '''
-def zip_string(input) :
-	_c = ""
+def zip_string(string) :
+	_char = ""
 	count = 0
 	result = ""
 
-	for c in input :
-		if c != _c :
-			_c = c
+	for char in string :
+		if char != _char :
+			_char = char
 			if count :
 				result += str(count)
-			result += c
+			result += char
 			count = 1
 		else :
 			count += 1
@@ -25,4 +25,4 @@ def zip_string(input) :
 
 	return result
 
-print(zip_string('aaabbcccccca')) # a3b2c6a1	
+print(zip_string('aaabbcharcharcharcharcharchara')) # a3b2char6a1
