99bb936120afb94bd59c4b6672db9bd678627e60
alstn2468 <alstn2468_@naver.com>
2018-05-21 13:34:16 +0900

    2018-05-21

---
+++ Chapter.7/예제/1.py
@@ -0,0 +1,23 @@
+# 정규 표현식 사용 전
+
+data = '''
+park 800905-1049118
+kim  700905-1059119
+'''
+
+result = []
+for line in data.split('\n') :
+
+    word_result = []
+
+    for word in line.split(' ') :
+
+        if len(word) == 14 and word[:6].isdigit() and word[7:].isdigit() :
+
+            word = word[:6] + '-' + '*******'
+
+        word_result.append(word)
+
+    result.append(' '.join(word_result))
+
+print('\n'.join(result))
---
+++ Chapter.7/예제/2.py
@@ -0,0 +1,12 @@
+# 정규 표현식 사용 후
+
+import re
+
+data = '''
+park 800905-1049118
+kim  700905-1059119
+'''
+
+pat = re.compile("(\d{6})[-]\d{7}")
+
+print(pat.sub('\g<1>-*******', data))
