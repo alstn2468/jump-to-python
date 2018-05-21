c690453fa58da5555b81aa2395b874065cc49391
alstn2468 <alstn2468_@naver.com>
2018-05-21 14:32:15 +0900

    2018-05-21

---
+++ Chapter.7/예제/11.py
@@ -0,0 +1,17 @@
+# MULTILINE, M
+
+import re
+
+p = re.compile('^python\s\w+')
+
+data = '''python one
+life is too short
+python two
+you need python
+python three'''
+
+print(p.findall(data)) # ['python one']
+
+p = re.compile('^python\s\w+', re.MULTILINE)
+
+print(p.findall(data)) # ['python one', 'python two', 'python three']
