a0a2731a6c5e6037b4347044d6b9a3944cf4f8d4
alstn2468 <alstn2468_@naver.com>
2018-06-12 16:02:57 +0900

    2018-06-12

---
+++ Chapter.7/Practice/5.py
@@ -0,0 +1,8 @@
+'''
+ElementTree를 이용하여 다음 XML 문서를 작성하고 파일에 저장해 보자.
+<blog date="20151231">
+    <subject>Why Python?</subject>
+    <author>Eric</author>
+    <content>Life is too short, You need Python!</content>
+</blog>    
+'''
