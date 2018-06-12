a9b67c9800fe3b4441f2161d945ccdef5ff23a59
alstn2468 <alstn2468_@naver.com>
2018-06-12 21:24:34 +0900

    2018-06-12

--- Chapter.7/Practice/5.py
+++ Chapter.7/Practice/5.py
@@ -4,5 +4,16 @@ ElementTree를 이용하여 다음 XML 문서를 작성하고 파일에 저장�
     <subject>Why Python?</subject>
     <author>Eric</author>
     <content>Life is too short, You need Python!</content>
-</blog>    
+</blog>
 '''
+
+from xml.etree.ElementTree import Element, SubElement, ElementTree
+
+blog = Element('blog')
+blog.attrib['date'] = '20151231'
+
+SubElement(blog, 'subject').text = 'Why Python?'
+SubElement(blog, 'author').text = 'Eric'
+SubElement(blog, 'content').text = 'Life is too short, You need Python!'
+
+ElementTree(blog).write('blog.xml')
